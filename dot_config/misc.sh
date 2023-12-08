22c3efce07ef3e6e7528b19deb50593002a3f697
Re'em Kishinevsky <Re'em Kishinevsky>
2023-12-08 20:11:19 +0200

    Added misc.sh to track configs that are system-wide

---
+++ dot_config/misc.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Gnome - switch input source with alt-shift
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Shift>Alt_L']"
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<Alt>Shift_L']"
+
