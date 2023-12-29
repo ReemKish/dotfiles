d309368334b2ff9d47cdac1ce9448b33c3cb183b
Re'em Kishinevsky <Re'em Kishinevsky>
2023-12-29 18:03:11 +0200

    update

--- dot_config/nvim/lua/user/keymaps.lua
+++ dot_config/nvim/lua/user/keymaps.lua
@@ -1,19 +1,24 @@
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
+-- Set leader to space.
 keymap("n", "<Space>", "", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- <C-Space> opens WhichKey immediately.
 keymap("n", "<C-Space>", "<cmd>WhichKey \\<space><cr>", opts)
 keymap("n", "<C-i>", "<C-i>", opts)
 
--- Better window navigation
-keymap("n", "<m-h>", "<C-w>h", opts)
-keymap("n", "<m-j>", "<C-w>j", opts)
-keymap("n", "<m-k>", "<C-w>k", opts)
-keymap("n", "<m-l>", "<C-w>l", opts)
-keymap("n", "<m-tab>", "<c-6>", opts)
+-- Better window navigation, with tmux interoperation.
+keymap("n", "<C-h>", ":NavigatorLeft<CR>", opts)
+keymap("n", "<C-j>", ":NavigatorDown<CR>", opts)
+keymap("n", "<C-k>", ":NavigatorUp<CR>", opts)
+keymap("n", "<C-l>", ":NavigatorRight<CR>", opts)
+keymap("n", "<C-tab>", ":NavigatorPrevious<CR>", opts)
+
+keymap({"n", "i"}, "<C-s>", "<ESC>:w<CR>", opts)  -- save
+keymap({"n", "i"}, "<C-q>", "<ESC>:q<CR>", opts)  -- quit
 
 keymap("n", "n", "nzz", opts)
 keymap("n", "N", "Nzz", opts)
@@ -22,9 +27,12 @@ keymap("n", "#", "#zz", opts)
 keymap("n", "g*", "g*zz", opts)
 keymap("n", "g#", "g#zz", opts)
 
--- Stay in indent mode
+-- Stay in indent mode when readjusting indentation.
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
+
+-- jk to exit insert mode
+keymap("i", "jk", "<ESC>")
 
 keymap("x", "p", [["_dP]])
 
